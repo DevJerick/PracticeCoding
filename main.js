5b24ac1132c5909d520ce49b386285c0bbdc0345
DevJerick <88653346+DevJerick@users.noreply.github.com>
2021-08-09 16:40:38 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+document.querySelector('.addTask').onclick = function (){
+	if (document.querySelector('input').value.length == 0) {
+		alert('Please Enter a Task');
+	}
+	else {
+		const userInput = document.querySelector('input').value;
+
+		document.querySelector('.tasks').innerHTML += `
+
+		<div class="task">
+		
+		<span ="class-name">
+		${userInput}
+		</span>
+
+		<button class="delete"><i class="fas fa-trash-alt"></i></button>
+
+		</div>
+
+		`;
+
+		var count = document.querySelectorAll('.task').length;
+
+		var current_tasks = document.querySelectorAll('.delete');
+
+		for (var i = 0; i < current_tasks.length; i++) {
+			current_tasks[i].onclick = function (){
+				this.parentNode.remove();
+			};
+		}
+
+		var tasks = document.querySelectorAll('.task');
+		for (i = 0; i < tasks.length; i++) {
+			tasks[i].onclick = function (){
+				this.classList.toggle('completed');
+			};
+		}
+
+		document.querySelector('input').value = '';
+		document.querySelector('.footer').innerHTML = `You have ${count} pending tasks`;
+	}
+};
